a0355760cee589dec4d2d2ee18ee8c2be21033e6
kim <0226yj@gmail.com>
2024-11-25 20:26:12 +0900

    chat_function1

--- Chat_project/server.js
+++ Chat_project/server.js
@@ -6,9 +6,23 @@ import ViteExpress from "vite-express";
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+      }
+});
 
-io.on('connection', () => { /* .... */ });
+io.on('connection', (client) => { /* .... */ 
+    
+    console.log(client.handshake.query);
+    
+    const connectedClientUsername = client.handshake.query.username;
+
+    console.log(`사용자 들어왔습니다. ${connectedClientUsername}`);
+    client.on('disconnect', () => {
+        console.log(`사용자 나갔습니다... ${connectedClientUsername}`);
+    });
+});
 server.listen(3000, () => { 
     /* ... */ 
     console.log('Server is listening on port 3000');
--- Chat_project/src/App.jsx
+++ Chat_project/src/App.jsx
@@ -2,9 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+// ES modules
+import { io } from "socket.io-client";
 function App() {
   const [count, setCount] = useState(0)
+
+  const [username, setUsername] = useState('');
+
+  const [socket, setSocket] = useState(null);
+
+  function connectToChatServer(){
+    console.log('connectToChatServer');
+    const _socket = io('http://localhost:3000', {
+      autoConnect: false,
+      query: {
+        username: username,
+      }
+    });
+    _socket.connect();
+    setSocket(_socket);
+  }
+
+  function disconnectToChatServer(){
+    console.log('disconnectToChatServer');
+    socket?.disconnect();
+  }
 
   return (
     <>
@@ -21,10 +43,19 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
+
+      <h1>유저 : {username}</h1>
+      <div className = 'card'>
+        <input value = {username} onChange = { e => setUsername(e.target.value)} />
+        <button onClick={() => connectToChatServer()}>
+           접속
+        </button>
+        <button onClick={() => disconnectToChatServer()}>
+           접속종료
+        </button>
+      </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
